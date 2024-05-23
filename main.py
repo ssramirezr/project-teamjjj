e3ebd02eb5dda2b26d0a84582a516e004d27d266
Juan Jara <jaracalle2041@gmail.com>
2024-05-23 12:11:35 -0500

    Solucionado el bug del epsilon

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def leer_gramatica() -> dict:
     return gramatica
 
 def first(cadena: str, gramatica: dict, producciones_pasadas) -> set:
-    if cadena[0] in producciones_pasadas:
+    if cadena[0] in producciones_pasadas and ord(cadena[0]) < 97:
         return set()
     
     if cadena[0] in CACHE_FIRST:
@@ -20,10 +20,19 @@ def first(cadena: str, gramatica: dict, producciones_pasadas) -> set:
     first_set = set()
     if cadena[0] in gramatica:
         for produccion in gramatica[cadena[0]]:
-            for elemento in first(produccion[0], gramatica, producciones_pasadas):
+            first_produccion = first(produccion[0], gramatica, producciones_pasadas)
+            if "e" in first_produccion:
+                for i in range(1, len(produccion), 1):
+                    first_produccion.remove("e")
+                    for elemento in first(produccion[i], gramatica, producciones_pasadas):
+                        first_produccion.add(elemento)
+                    if "e" not in first_produccion:
+                        break
+            for elemento in first_produccion:
                 first_set.add(elemento)
     else:
         return cadena[0]
+            
     
     CACHE_FIRST[cadena[0]] = first_set
     return first_set
